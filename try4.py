4dfea5f508b0d86505137dba7089ca44a4230f71
tiaraa2436381 <riwehtiara@gmail.com>
2026-08-17 22:24:40 +0700

    try4.py

---
+++ try4.py
@@ -0,0 +1,13 @@
+nilai = 7
+
+nilai += 6
+print("sisa nilai setalah di tambahkan:",nilai)
+
+nilai -= 4
+print("sisa nilai setalah di kurang:",nilai)
+
+nilai *=3
+print("sisa nilai setalah dikalikan:",nilai)
+
+nilai /=2
+print("sisa nilai setelah dibagi:",nilai)
