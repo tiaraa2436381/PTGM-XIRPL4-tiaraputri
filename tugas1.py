3b15d480504a915ec3f3b1c17f1393b3a6ce70ae
tiaraa2436381 <riwehtiara@gmail.com>
2026-08-17 22:25:53 +0700

    tugas1.py

---
+++ tugas1.py
@@ -0,0 +1,16 @@
+roti = 100
+
+roti += 50
+print("penambahan roti:",roti)
+
+roti *= 3
+print("dikalikan pesanan pelanggan:",roti)
+
+roti -= 120
+print("dikurangi pengiriman ke pelanggan:",roti)
+
+roti /= 4
+print("di bagi kedalam 4 etalase:",roti)
+
+roti += 10
+print("ditambahkan roti setiap etalase:",roti)
