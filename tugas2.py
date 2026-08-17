76330ebae2844381c3e066d4fd260ca1b6ca32e8
tiaraa2436381 <riwehtiara@gmail.com>
2026-08-17 22:26:45 +0700

    tugas2.py

---
+++ tugas2.py
@@ -0,0 +1,30 @@
+a = 108
+b = 183
+c = 41
+print("")
+d = a & c
+print("nilai:",a)
+print("nilai:",c)
+print("nilai bitwise AND=",d)
+print("")
+e = b | a
+print("nilai :",b)
+print("nilai :",a)
+print("nilai bitwise OR=",e)
+print("")
+f = c ^ b
+print("nilai :",c)
+print("nilai :",b)
+print("nilai bitwise XOR=",f)
+print("")
+g = a & b | c
+print("nilai :",a)
+print("nilai :",b)
+print("nilai :",c)
+print("nilai bitwise AND + OR=",g)
+print("")
+h = c ^ a ^ b
+print("nilai :",c)
+print("nilai :",a)
+print("nilai :",b)
+print("nilai bitwise XOR=",h)
