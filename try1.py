d0d3607d81d4f2b7239d18fd97dcead41c194f19
tiaraa2436381 <riwehtiara@gmail.com>
2026-08-17 22:13:44 +0700

    try1.py

---
+++ try1.py
@@ -0,0 +1,12 @@
+nama = "tiara putri aprilliani"
+umur = "16 tahun"
+hobi = "memasak"
+perempuan = True
+berat_badan = 50.2
+
+print("===Biodata===")
+print("nama:",nama)
+print("umur:",umur)
+print("hobi:",hobi)
+print("perempuan:", perempuan)
+print("berat_badan:",berat_badan)
